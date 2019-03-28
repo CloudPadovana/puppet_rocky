cc8186c74235f8e14badda0c47f95b2b75b17b92
root <root@cld-foreman.cloud.pd.infn.it>
2019-03-28 16:34:51 +0100

    Massimo Sgaravatto: trying to be more or less compliant with doc

--- controller_rocky/manifests/configure_ec2.pp
+++ controller_rocky/manifests/configure_ec2.pp
@@ -5,6 +5,16 @@ class controller_rocky::configure_ec2 inherits controller_rocky::params {
 # - popola il file /etc/ec2api/ec2api.conf
 # 
 ### FF da queens per i metadata deve modificare anche il file /etc/neutron/metadata_agent.ini
+
+
+file { ['/var/lib/ec2-api',
+       '/var/lib/ec2-api/tmp']:
+      ensure => 'directory',
+      owner  => 'ec2api',
+      group  => 'ec2api',
+      mode   => '0750',
+    }
+
   
 define do_config ($conf_file, $section, $param, $value) {
              exec { "${name}":
@@ -36,11 +46,14 @@ define remove_config ($conf_file, $section, $param, $value) {
    do_config { 'ec2_external_network': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'external_network', value => $controller_rocky::params::ec2_external_network, }
    do_config { 'ec2_ssl_ca_file': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'ssl_ca_file', value => $controller_rocky::params::cafile, }
    do_config { 'ec2_log_file': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'log_file', value => $controller_rocky::params::ec2_log_file, }
-   do_config { 'ec2_full_vpc_support': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'full_vpc_support', value => $controller_rocky::params::ec2_full_vpc_support, }
+   do_config { 'ec2_logging_context_format_string': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'logging_context_format_string', value => $controller_rocky::params::ec2_logging_context_format_string, } 
+   do_config { 'ec2_cinder_service_type': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'cinder_service_type', value => $controller_rocky::params::ec2_cinder_service_type, } 
+       do_config { 'ec2_full_vpc_support': conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'full_vpc_support', value => $controller_rocky::params::ec2_full_vpc_support, }
    do_config { 'keystone_ec2_tokens_url':  conf_file => '/etc/ec2api/ec2api.conf', section => 'DEFAULT', param => 'keystone_ec2_tokens_url', value => "${controller_rocky::params::ec2_keystone_url}/ec2tokens",}
    do_config { 'ec2_db': conf_file => '/etc/ec2api/ec2api.conf', section => 'database', param => 'connection', value => $controller_rocky::params::ec2_db, }
-   do_config { 'ec2_auth_uri': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'auth_uri', value => $controller_rocky::params::auth_url, }   
-   do_config { 'ec2_auth_url': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'auth_url', value => $controller_rocky::params::auth_url, }   
+#   do_config { 'ec2_auth_uri': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'auth_uri', value => $controller_rocky::params::auth_url, }   
+   do_config { 'ec2_www_authenticate_uri': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'www_authenticate_uri', value => $controller_rocky::params::ec2_keystone_url, }   
+   do_config { 'ec2_auth_url': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'auth_url', value => $controller_rocky::params::ec2_keystone_url, }   
    do_config { 'ec2_user': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'username', value => $controller_rocky::params::ec2_user, }
    do_config { 'ec2_password': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'password', value => $controller_rocky::params::ec2_password, }
    do_config { 'ec2_tenant_name': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'project_name', value => $controller_rocky::params::project_name, }
@@ -50,4 +63,14 @@ define remove_config ($conf_file, $section, $param, $value) {
    do_config { 'ec2_cafile': conf_file => '/etc/ec2api/ec2api.conf', section => 'keystone_authtoken', param => 'cafile', value => $controller_rocky::params::cafile, }
    do_config { 'ec2_nova_metadata_ip': conf_file => '/etc/ec2api/ec2api.conf', section => 'metadata', param => 'nova_metadata_ip', value => $controller_rocky::params::ec2_nova_metadata_ip, }
    do_config { 'ec2_auth_ca_cert': conf_file => '/etc/ec2api/ec2api.conf', section => 'metadata', param => 'auth_ca_cert', value => $controller_rocky::params::cafile, }
+
+   do_config { 'ec2_cache_backend': conf_file => '/etc/ec2api/ec2api.conf', section => 'cache', param => 'backend', value => $controller_rocky::params::ec2_cache_backend, } 
+   do_config { 'ec2_cache_enabled': conf_file => '/etc/ec2api/ec2api.conf', section => 'cache', param => 'enabled', value => $controller_rocky::params::ec2_cache_enabled, } 
+
+   do_config { 'ec2_lock_path': conf_file => '/etc/ec2api/ec2api.conf', section => 'oslo_concurrency', param => 'lock_path', value => $controller_rocky::params::ec2_lock_path, } 
+       
+
+
+
+
 }
