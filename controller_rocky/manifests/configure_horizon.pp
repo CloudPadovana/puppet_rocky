9d6860c7c69dc0b08bd7d89f5442c2ea1e4ec2ef
root <root@cld-foreman.cloud.pd.infn.it>
2019-07-17 08:55:42 +0200

    PA: disabled port_80_closed

--- controller_rocky/manifests/configure_horizon.pp
+++ controller_rocky/manifests/configure_horizon.pp
@@ -32,10 +32,10 @@ class controller_rocky::configure_horizon inherits controller_rocky::params {
     mode     => '0644',
   }
 
-  exec { "port_80_closed":
-    command => "/usr/bin/sed -i -e 's|^Listen\\s*80\\s*|#Listen 80|g' /etc/httpd/conf/httpd.conf" ,
-    unless  => "/bin/grep '#Listen\\s*80\\s*$' /etc/httpd/conf/httpd.conf",
-  }
+#  exec { "port_80_closed":
+#    command => "/usr/bin/sed -i -e 's|^Listen\\s*80\\s*|#Listen 80|g' /etc/httpd/conf/httpd.conf" ,
+#    unless  => "/bin/grep '#Listen\\s*80\\s*$' /etc/httpd/conf/httpd.conf",
+#  }
 
 
   ############################################################################
