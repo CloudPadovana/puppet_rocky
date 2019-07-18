93d70020f62d1baa1dcb364848c7d6df06767238
root <root@cld-foreman.cloud.pd.infn.it>
2019-07-18 06:39:25 +0200

    fix pci stuff

--- compute_rocky/manifests/nova.pp
+++ compute_rocky/manifests/nova.pp
@@ -194,7 +194,7 @@ do_config { 'nova_enable_proxy_headers_parsing': conf_file => '/etc/nova/nova.co
            conf_file => '/etc/nova/nova.conf',
            section   => 'pci',
            param     => 'alias',
-           values    => [ "$compute_ocata::params::pci_titanxp_VGA", "$compute_ocata::params::pci_titanxp_SND", "$compute_ocata::params::pci_quadro_VGA", "$compute_ocata::params::pci_quadro_Audio", "$compute_ocata::params::pci_quadro_USB", "$compute_ocata::params::pci_quadro_SerialBus", "$compute_ocata::params::pci_geforcegtx_VGA", "$compute_ocata::params::pci_geforcegtx_SND"  ],
+           values    => [ "$compute_rocky::params::pci_titanxp_VGA", "$compute_rocky::params::pci_titanxp_SND", "$compute_rocky::params::pci_quadro_VGA", "$compute_rocky::params::pci_quadro_Audio", "$compute_rocky::params::pci_quadro_USB", "$compute_rocky::params::pci_quadro_SerialBus", "$compute_rocky::params::pci_geforcegtx_VGA", "$compute_rocky::params::pci_geforcegtx_SND"  ],
            #values    => [ "$compute_rocky::params::pci_alias_1", "$compute_rocky::params::pci_alias_2" ],
          }
          
