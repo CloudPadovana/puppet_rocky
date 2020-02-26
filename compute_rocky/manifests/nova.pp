8078f125c4642e88e37579fd8a2fb3f5826d917a
sgaravat <massimo.sgaravatto@pd.infn.it>
2020-02-26 16:27:16 +0100

    Support for V100

--- compute_rocky/manifests/nova.pp
+++ compute_rocky/manifests/nova.pp
@@ -228,6 +228,27 @@ compute_rocky::nova::do_config { 'nova_enable_proxy_headers_parsing': conf_file 
    
 }
 
+# GPU specific setting and some setting for better performance for SSD disk for cld-np-gpu-01
+ if ($::mgmt_ip == "192.168.60.128") {
+  compute_rocky::nova::do_config { 'pci_passthrough_whitelist': conf_file => '/etc/nova/nova.conf', section => 'pci', param => 'passthrough_whitelist', value => $compute_rocky::params::pci_passthrough_whitelist, }
+
+   compute_rocky::nova::do_config_list { "pci_alias":
+           conf_file => '/etc/nova/nova.conf',
+           section   => 'pci',
+           param     => 'alias',
+           values    => [ "$compute_rocky::params::pci_v100"   ],
+         }
+
+   compute_rocky::nova::do_config_list { "preallocate_images":
+           conf_file => '/etc/nova/nova.conf',
+           section   => 'DEFAULT',
+           param     => 'preallocate_images',
+           values    => [ "$compute_rocky::params::nova_preallocate_images"   ],
+         }
+         
+   
+}
+
 
 
 #####
